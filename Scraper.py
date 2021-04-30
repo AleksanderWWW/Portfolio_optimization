207e68fd426dfe3590bcd785d5e30a16c802c455
AleksanderWWW <alwojnarowicz@gmail.com>
2021-04-30 20:21:48 +0200

    Build a scraping engine to scrape the stock data from stooq

--- Scraper.py
+++ Scraper.py
@@ -1,10 +1,64 @@
 import requests
+from bs4 import BeautifulSoup
+from math import ceil
+import pandas as pd
 
 
 class Scraper:
-    pass
+    base_url = "https://stooq.pl/t/?i=513"
+
+    def __init__(self):
+        self.all_dfs = []
+
+    @staticmethod
+    def calculate_pagination(number_records):
+        """This function will calculate how many pages need to be scraped, based on the total number of records,
+        since we know that there are max 100 records per page"""
+        n = ceil(number_records/100)
+        return n
+
+    def create_soup(self, page_num):
+        r = requests.get(self.base_url+"&v=0&l="+str(page_num))
+        soup = BeautifulSoup(r.text, 'lxml')
+        return soup
+
+    def get_num_records(self):
+        r = requests.get(self.base_url)
+        soup = BeautifulSoup(r.text, 'lxml')
+        text = soup.find("td", {"id": "f13"}).text
+        record_num = text.split("|")[0].split(" ")[-2]
+        return soup, int(record_num)
+
+    @staticmethod
+    def extract_table(page_soup):
+        data = []
+        table = page_soup.find("table", {"id": "fth1"})
+        header = ["Symbol",	"Nazwa", "Kurs", "Zmiana1", "Zmiana1", "Wolumen"]
+        table = table.find("tbody")
+        for row in table.find_all("tr"):
+            cell_list = [cell.text for cell in row.find_all('td')][:-2]
+            data.append(cell_list)
+        df = pd.DataFrame(data, columns=header)
+        return df
+
+    @staticmethod
+    def merge_data_frames(dfs):
+        df = pd.concat(dfs).reset_index(drop=True)
+        return df
+
+    def scrape(self):
+        soup, num = self.get_num_records()
+        num_of_pages = self.calculate_pagination(num)
+        df1 = self.extract_table(soup)
+        self.all_dfs.append(df1)
+        for i in range(2, num_of_pages+1):
+            page_soup = self.create_soup(i)
+            df = self.extract_table(page_soup)
+            self.all_dfs.append(df)
+        result = self.merge_data_frames(self.all_dfs)
+        result.to_excel("Notowania_giełdowe.xlsx", index=False)
 
 
-class PageObjModel:
-    pass
-
+if __name__ == "__main__":
+    scraper = Scraper()
+    scraper.scrape()
