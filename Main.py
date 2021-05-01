a702cdb28599016ce6cf433f017af65abf914618
AleksanderWWW <alwojnarowicz@gmail.com>
2021-05-01 13:05:54 +0200

    Create the inside of the main function

--- Main.py
+++ Main.py
@@ -1,10 +1,15 @@
-import subprocess
 from GUi import Gui
 from Scraper import Scraper
+from rtrigger import RScriptTrigger
 
 
 def main():
-    pass
+    path = "project.r"
+    scraper = Scraper()
+    r_trigger = RScriptTrigger(path)
+
+    gui = Gui(title="Portfolio Optimization", scraper_obj=scraper, rtrigger_obj= r_trigger)
+    gui.run()
 
 
 if __name__ == "__main__":
