bc43e7c51c5d0645842b4cd61bbecb556d54bdcf
AleksanderWWW <alwojnarowicz@gmail.com>
2021-05-14 10:43:46 +0200

    Delete unused methods and imports

--- Scraper.py
+++ Scraper.py
@@ -1,10 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-from math import ceil
 import pandas as pd
 import datetime as dt
 import pandas_datareader as pdr
-import multiprocessing as mp
 from concurrent.futures import ThreadPoolExecutor as Executor
 
 
@@ -18,14 +16,6 @@ class Scraper:
         self.data = pd.DataFrame()
         self.tickers = []
 
-    # def get_tickers(self):
-    #     html = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    #     soup = BeautifulSoup(html.text, 'lxml')
-    #     table = soup.find('table', {'class': 'wikitable sortable'})
-    #     for row in table.findAll('tr')[1:]:
-    #         ticker = row.findAll('td')[0].text
-    #         ticker = ticker[:-1]
-    #         self.tickers.append(ticker)
     def get_tickers(self):
         html = requests.get("https://stooq.pl/q/i/?s=^dji")
         soup = BeautifulSoup(html.text, 'lxml')
@@ -47,59 +37,11 @@ class Scraper:
             executor.map(self.get_time_series, self.tickers)
         self.data = self.merge_data_frames(self.all_dfs)
         self.data = self.data.sort_index(axis=1)
-        #pool = mp.Pool(mp.cpu_count())
-        #[pool.apply(self.get_time_series, args=(ticker,)) for ticker in self.tickers]
-        #pool.close()
-        #for ticker in self.tickers[:5]:
-            #self.get_time_series(ticker=ticker)
-
-    @staticmethod
-    def calculate_pagination(number_records):
-        """This function will calculate how many pages need to be scraped, based on the total number of records,
-        since we know that there are max 100 records per page"""
-        n = ceil(number_records/100)
-        return n
-
-    def create_soup(self, page_num):
-        r = requests.get(self.base_url+"&v=0&l="+str(page_num))
-        soup = BeautifulSoup(r.text, 'lxml')
-        return soup
-
-    def get_num_records(self):
-        r = requests.get(self.base_url)
-        soup = BeautifulSoup(r.text, 'lxml')
-        text = soup.find("td", {"id": "f13"}).text
-        record_num = text.split("|")[0].split(" ")[-2]
-        return soup, int(record_num)
-
-    @staticmethod
-    def extract_table(page_soup):
-        data = []
-        table = page_soup.find("table", {"id": "fth1"})
-        header = ["Symbol",	"Nazwa", "Kurs", "Zmiana1", "Zmiana2", "Wolumen"]
-        table = table.find("tbody")
-        for row in table.find_all("tr"):
-            cell_list = [cell.text for cell in row.find_all('td')][:-2]
-            data.append(cell_list)
-        df = pd.DataFrame(data, columns=header)
-        return df
 
     @staticmethod
     def merge_data_frames(dfs):
         df = pd.concat(dfs, axis=1)
         return df
-
-    def scrape(self):
-        soup, num = self.get_num_records()
-        num_of_pages = self.calculate_pagination(num)
-        df1 = self.extract_table(soup)
-        self.all_dfs.append(df1)
-        for i in range(2, num_of_pages+1):
-            page_soup = self.create_soup(i)
-            df = self.extract_table(page_soup)
-            self.all_dfs.append(df)
-        result = self.merge_data_frames(self.all_dfs)
-        return result
 
 
 if __name__ == "__main__":
