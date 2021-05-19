ca5ca234b61c44ee775705caef9f9dff16f7252c
AleksanderWWW <alwojnarowicz@gmail.com>
2021-05-19 19:45:48 +0200

    adjust algorithm funcs; adjust py objects conversion for cov matrix and return vector

--- rtrigger.py
+++ rtrigger.py
@@ -8,17 +8,19 @@ class RScriptTrigger:
     def __init__(self, script_path):
         self.script_path = script_path
         self.r = ro.r
-        self.r_df = None
-        self.result_dict = None
+        self.r_cov_df = None
+        self.r_ind_er = None
+        self.param_dict = None
 
     def source_r_script(self):
         self.r['source'](self.script_path)
 
-    def add_df(self, df):
-        self.r_df = self.df_to_r_object(df)
+    def add_py_objects(self, cov_matrix, ind_er_vector):
+        self.r_cov_df = self.df_to_r_object(cov_matrix)
+        self.r_ind_er = self.df_to_r_object(ind_er_vector)
 
-    def add_result_dict(self, r_dict: dict):
-        self.result_dict = r_dict
+    def add_result_dict(self, p_dict: dict):
+        self.param_dict = p_dict
 
     @staticmethod
     def df_to_r_object(df):
@@ -26,16 +28,12 @@ class RScriptTrigger:
             r_from_pd_df = ro.conversion.py2rpy(df)
         return r_from_pd_df
 
-    def simAnnealing(self):
-        pass
+    def simulated_annealing(self):
+        params = self.param_dict["Simulated_annealing"]
+        sim_ann = ro.globalenv["simulated_annealing"]
 
-    def tabuSearch(self):
-        pass
-
-    def genAlgorithm(self):
-        pass
-
-    def antColony(self):
+    def particle_swarm(self):
+        params = self.param_dict["Particle_swarm"]
         pass
 
     def optimize(self):
